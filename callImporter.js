71c0ce03ddc835a1ff514879e8b85202ae723032
Koen Verduijn <koenv1985@gmail.com>
2023-06-29 16:52:25 +0200

    All possible data fields & updated logging comment

--- callImporter.js
+++ callImporter.js
@@ -17,7 +17,7 @@ function triggerAircallCallData() {
 };
 
 // to get as much historic data into sheet at once
-async function monthAircallCallDataImporter() {
+async function historicAircallCallDataImporter() {
   /* get specific date range */
   const dateFrom = new Date("2023-01-20").getTime() / 1000;
   const dateTo = new Date("2023-01-31").getTime() / 1000;
@@ -54,131 +54,136 @@ async function getAircallData(apiUrl, object) {
 async function getAll(object, dateFrom, dateTo, dataOverwrite) {
   const objectName =
     object.charAt(0).toUpperCase() + object.substring(1, object.length);
-  let dataSheet = SpreadsheetApp.getActive().getSheetByName(
-    objectName + " Data"
-  );
+  let dataSheet = SpreadsheetApp.getActive().getSheetByName(object + " data");
   try {
     if (!dataSheet) {
-      SpreadsheetApp.getActive().insertSheet(objectName + " Data");
-      dataSheet = SpreadsheetApp.getActive().getSheetByName(
-        objectName + " Data"
-      );
-    }
+      SpreadsheetApp.getActive().insertSheet(object + " data");
+      dataSheet = SpreadsheetApp.getActive().getSheetByName(object + " data");
+      Logger.log("New Data Sheet created: " + object + " data")
+    };
     if (dataOverwrite === true) {
       Logger.log("Removing all " + object + " data");
       dataSheet.clear();
       if (dataSheet.getMaxRows() > 100)
         dataSheet.deleteRows(100, dataSheet.getMaxRows() - 100);
-    }
+    };
     let dateFilter = "";
     if (object === "calls") dateFilter = "&from=" + dateFrom + "&to=" + dateTo;
     let page = 1;
-    // Default options are marked with *
     let objectRecords = [];
     let nextUrl = "";
     let data = null;
-
     while (nextUrl != null) {
       let apiUrl =
         baseUrl + object + "?per_page=50&order=asc&page=" + page + dateFilter;
       data = await getAircallData(apiUrl, object);
       objectRecords = data[object];
       const pages = Math.ceil(data["meta"]["total"] / 50);
-      Logger.log(
-        "collecting " +
-          data["meta"]["total"] +
-          " " +
-          object +
-          " data from " +
-          page +
-          "/" +
-          pages
-      );
-      if (objectRecords.length === 0) Logger.log("No " + object + " available");
-      else if (page === 1)
-        Logger.log(
-          "Importing " +
-            data["meta"]["total"] +
-            " " +
-            object +
-            " data from " +
-            page +
-            "/" +
-            pages +
-            "\nThis will take roughly " +
-            Math.ceil((2 * pages) / 60) +
-            " minutes"
-        );
+      if(data["meta"]["total"] > 9999) {
+        Logger.log("More than " + data["meta"]["total"] + " " + object + " data. Please reduce the time period");      
+        break;
+      };
+      if(objectRecords.length === 0) {
+        Logger.log("No " + object + " available");
+        break;
+      };
+      if(page === 1)
+        Logger.log("Importing " +  data["meta"]["total"] +  " " +  object + " data ("+ new Date(dateFrom * 1000).toLocaleString() + " - " + new Date(dateTo * 1000).toLocaleString() + ") data from " + pages +  ((pages > 1) ? " pages." : " page.") +"\nThis will take roughly " +  Math.ceil((2 * pages) / 60) +  " minutes");
+      Logger.log("collecting " + data["meta"]["total"] + " " + object +" from " + page + "/" + pages);
       let finalRecords = [];
-      if (dataOverwrite === true && page === 1) {
-        const cols = Object.keys(objectRecords[0]);
-        const printCols = [];
-        cols.forEach(function (col) {
-          printCols.push([col]);
-        });
-        printCols.push(["dateTime"]);
-        printCols.push(["date"]);
-        printCols.push(["talkingTime"]);
-        printCols.push(["waitingTime"]);
-        printCols.push(["line"]);
-        printCols.push(["tags"]);
-        printCols.push(["agent"]);
-        printCols.push(["week"]);
-        printCols.push(["month"]);
-        // set column headers
-        if (cols.length > 0) {
-          finalRecords.push(printCols);
-        }
-      }
+      if (
+				(dataOverwrite === true && page === 1) ||
+				SpreadsheetApp.getActive()
+					.getSheetByName(object + " data")
+					.getLastRow() === 0
+			) {
+				const cols = Object.keys(objectRecords[0]);
+				const printCols = [];
+				cols.forEach(function (col) {
+					printCols.push([col]);
+				});
+        // additional columns
+				printCols.push(["dateTime"]);
+				printCols.push(["date"]);
+				printCols.push(["week"]);
+				printCols.push(["month"]);
+				printCols.push(["talkingTime"]);
+				printCols.push(["waitingTime"]);
+				printCols.push(["number"]);
+				printCols.push(["numberCountryCode"]);
+				printCols.push(["tags"]);
+				printCols.push(["agent"]);
+				printCols.push(["agentEmail"]);
+				printCols.push(["agentNumber"]);
+				printCols.push(["team"]);
+				// set column headers
+				if (cols.length > 0) {
+					finalRecords.push(printCols);
+				}
+			};
       for (r = 0; r < objectRecords.length; r++) {
         let recordRow = [];
         const recordFields = Object.values(objectRecords[r]);
         for (let rf = 0; rf < 26; rf++) recordRow.push([recordFields[rf]]);
+        // formulas or specific data for the additional columns
         recordRow.push(["=EPOCHTODATE(F:F)"]);
-        recordRow.push(["=LEFT(AA:AA;10)"]);
-        recordRow.push(['=IF(D:D<>"done";0;IF(G:G<>"";H:H-G:G;0))']);
-        recordRow.push(['=IF(D:D<>"done";0;IF(G:G<>"";G:G-F:F;I:I))']);
-        recordRow.push(objectRecords[r]["number"]["name"]);
+				recordRow.push(["=LEFT(AA:AA;10)"]);
+				recordRow.push(["=WEEKNUM(AB:AB)"]);
+				recordRow.push(["=MONTH(AB:AB)"]);
+				recordRow.push(['=IF(D:D<>"done";0;IF(G:G<>"";H:H-G:G;0))']);
+				recordRow.push(['=IF(D:D<>"done";0;IF(G:G<>"";G:G-F:F;I:I))']);
+				recordRow.push(objectRecords[r]["number"]["name"]);
+        recordRow.push((objectRecords[r]["number"] != null && objectRecords[r]["number"]["country"] != null) ? objectRecords[r]["number"]["country"] : "");
         let tags = "";
-        if (objectRecords[r]["tags"].length != 0) {
-          for (let t = 0; t < objectRecords[r]["tags"].length; t++) {
-            if (t === 0) tags = objectRecords[r]["tags"][t]["name"];
-            else tags = tags + ", " + objectRecords[r]["tags"][t]["name"];
-          }
-        }
-        recordRow.push([tags]);
-        let agent = "";
-        if (objectRecords[r]["user"] != null)
-          agent = objectRecords[r]["user"]["name"];
-        recordRow.push([agent]);
-        recordRow.push(["=WEEKNUM(AB:AB)"]);
-        recordRow.push(["=MONTH(AB:AB)"]);
+				if (objectRecords[r]["tags"].length != 0) {
+					for (let t = 0; t < objectRecords[r]["tags"].length; t++) {
+						if (t === 0) tags = objectRecords[r]["tags"][t]["name"];
+						else tags = tags + ", " + objectRecords[r]["tags"][t]["name"];
+					}
+				}
+				recordRow.push([tags]);
+        let agentName = "";
+        let agentEmail = "";
+        if (objectRecords[r]["user"] != null) {
+          // console.log(objectRecords[r]["user"]);
+          agentEmail = objectRecords[r]["user"]["email"];
+          agentName = objectRecords[r]["user"]["name"];
+        };
+        recordRow.push([agentName]);
+        recordRow.push([agentEmail]);
+        recordRow.push((objectRecords[r]["number"] != null && objectRecords[r]["number"]["digits"] != null) ? objectRecords[r]["number"]["digits"] : "");
+        let teamName = "";
+        if(objectRecords[r]["teams"].length > 0) {
+          // console.log(objectRecords[r]["teams"]);
+          teamName = objectRecords[r]["teams"][0]["name"];
+        };
+        recordRow.push([teamName]);
         finalRecords.push(recordRow);
       }
       const numberOfRows =
-        SpreadsheetApp.getActive()
-          .getSheetByName(objectName + " Data")
-          .getLastRow() === 0
-          ? 1
-          : SpreadsheetApp.getActive()
-              .getSheetByName(objectName + " Data")
-              .getLastRow() + 1;
+				SpreadsheetApp.getActive()
+					.getSheetByName(object + " data")
+					.getLastRow() === 0
+					? 1
+					: SpreadsheetApp.getActive()
+							.getSheetByName(object + " data")
+							.getLastRow() + 1;
       // Logger.log('writing from row: '+numberOfRows);
       SpreadsheetApp.getActive()
-        .getSheetByName(objectName + " Data")
-        .getRange(
-          numberOfRows,
-          1,
-          finalRecords.length,
-          Object.keys(objectRecords[0]).length + 9
-        )
-        .setValues(finalRecords);
+				.getSheetByName(object + " data")
+				.getRange(
+					numberOfRows,
+					1,
+					finalRecords.length,
+					Object.keys(objectRecords[0]).length + 13
+				)
+				.setValues(finalRecords);
       page += 1;
       nextUrl = data["meta"]["next_page_link"];
-    }
-    Logger.log("Importing " + object + " data complete!");
+    };
+    if(objectRecords.length > 1) Logger.log("Importing " + object + " data complete!");
   } catch (error) {
     // deal with any errors
     Logger.log(error);
   }
-};
+}
