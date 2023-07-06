a42ad0b54136ea3182885fef3c8a5db4f5c6faad
Koen Verduijn <koenv1985@gmail.com>
2023-07-06 10:35:34 +0200

    Support role and check API credentials

--- userImporter.js
+++ userImporter.js
@@ -1,8 +1,14 @@
+function onOpen() {
+	ui.createMenu("🚀 Aircall 🚀")
+		.addItem("Upload Users", "getUserData")
+		.addToUi();
+};
+
 // grab user data from cells
 async function getUserData() {
 	const userTab = SpreadsheetApp.getActive().getSheetByName("Users");
 	const userData = userTab
-		.getRange(2, 1, userTab.getLastRow() - 1, 3)
+		.getRange(2, 1, userTab.getLastRow() - 1, 4)
 		.getValues();
 	// Logger.log(userData);
 	for (let u = 0; u < userData.length; u++) {
@@ -10,6 +16,7 @@ async function getUserData() {
 			firstName: userData[u][0],
 			lastName: userData[u][1],
 			email: userData[u][2],
+			role: userData[u][3],
 		};
 		const createUser = await createRecord("user", userRecord);
 		Logger.log("created: " + createUser);
@@ -19,15 +26,24 @@ async function getUserData() {
 
 // create users in Aircall via API
 async function createRecord(object, record) {
+  if (apiId == null)
+		ui.alert(
+			"please add the Aircall API ID to the Google Apps Script Properties"
+		);
+	if (apiToken == null)
+		ui.alert(
+			"please add the Aircall API Token to the Google Apps Script Properties"
+		);	
 	if (object != "user" && object != "tag" && object != "contact")
-		ui.alert("please provide correct object. " + object + " is not valid");
+	ui.alert(
+		"please provide correct object. " + object + " is not valid"
+	);
 	else {
 		const payloadBody = {
 			email: record["email"],
 			first_name: record["firstName"],
 			last_name: record["lastName"],
-			is_admin: false,
-			role_ids: ["agent"],
+			role_ids: record["role"],
 		};
 		const res = await UrlFetchApp.fetch(baseUrl + "users", {
 			method: "POST", // *GET, POST, PUT, DELETE, etc.
@@ -43,6 +59,6 @@ async function createRecord(object, record) {
 			ui.alert("Error in creating user: " + res.getContentText());
 		else {
 			return res.getContentText();
-		};
-	};
-};
+		}
+	}
+}
